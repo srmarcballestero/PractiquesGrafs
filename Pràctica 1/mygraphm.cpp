b7e1c691c48cdc3f47c8d69a13562073333e7cd5
srmarcballestero <marconiballestero@gmail.com>
2020-10-08 21:55:37 +0200

    Pràctica 1

--- Pràctica 1/mygraphm.cpp
+++ Pràctica 1/mygraphm.cpp
@@ -54,9 +54,9 @@ vector<vector<int>> graphM_bipartite_complete (int n1, int n2)
 vector<vector<int>> graphM_star (int n)
 {
   vector<vector<int>> SnM (n+1, vector<int>(n+1, 0));
-//
-// ...
-//
+
+  for (int i = 1; i < n; ++i)
+    SnM[0][i] = SnM[i][0] = 1;
 
   return SnM;
 }
@@ -67,9 +67,47 @@ vector<vector<int>> graphM_star (int n)
 vector<vector<int>> graphM_wheel (int n)
 {
   vector<vector<int>> WnM (n+1, vector<int>(n+1, 0));
-//
-// ...
-//
+
+  for (int i = 0; i < n; ++i)
+  {
+    WnM[i][i+1] = WnM[i+1][i] = 1;
+    WnM[0][i+1] = WnM[i+1][0] = 1;
+  }
+  WnM[0][n] = WnM[n][0] = 1;
 
   return WnM;
+}
+
+//
+//  Grid graph to adjacency matrix
+//
+vector<vector<int>> graphM_grid (int n1, int n2)
+{
+  vector<vector<int>> Gn1n2M (n1*n2, vector<int>(n1*n2, 0));
+
+  for (int i = 0; i < n1; ++i)
+    for (int j = n1; j < n1+n2; ++j)
+    {
+      int v = i*n2 + j;
+      if (j)
+        Gn1n2M[v][v+1] = Gn1n2M[v+1][v] = 1;
+      if (i)
+        Gn1n2M[v][v+j] = Gn1n2M[v+j][v] = 1;
+    }
+
+  return Gn1n2M;
+}
+
+
+int main ()
+{
+  vector<vector<int>> W4 = graphM_wheel(3);
+
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; ++j)
+      cout << W4[i][j];
+    cout << endl;
+  }
+
+  return 0;
 }
