513754d34574aa000cb730845d32bbff5ef6dc6f
srmarcballestero <marconiballestero@gmail.com>
2020-12-27 15:47:14 +0100

    Pràctica 4

--- GPrc4/graph.h
+++ GPrc4/graph.h
@@ -1,3 +1,30 @@
 // NOM: Marc Ballestero Ribó.
 // GRUP: M1a.
-// DATA d'ENTREGA: 28/11/2020.
+// DATA d'ENTREGA: 13/01/2021.
+
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+typedef unsigned int vertex;
+typedef unsigned int edge;
+typedef unsigned int index;
+typedef unsigned int color;
+typedef vector<vector<vertex>> graph;
+graph graph_complete(index n);
+graph graph_bipartite_complete(index n1, index n2);
+graph graph_cycle(index n);
+graph graph_star(index n);
+graph graph_wheel(index n);
+graph graph_knight(index n1, index n2);
+graph graph_read(string fname);
+void graph_write(graph &G, ofstream &fout);
+void chess_graph_write(graph &G, index n1, index n2, ofstream &fout);
+bool HamiltonianCycle(graph &G, ofstream &fout);
+index HamiltonianCycles(graph &G, vector<color> &Gc, ofstream &fout);
+color MinimalVertexColoring(graph &G, vector<color> &Gc, ofstream &fout);
--- GPrc4/graphHC.cpp
+++ GPrc4/graphHC.cpp
@@ -1,3 +1,104 @@
 // NOM: Marc Ballestero Ribó.
 // GRUP: M1a.
-// DATA d'ENTREGA: 28/11/2020.
+// DATA d'ENTREGA: 13/01/2021.
+
+#include "graph.h"
+#include "wgraph.h"
+
+//
+//  Robert and Flores method for a hamiltonian cycle.
+//
+bool HamiltonianCycle (graph &G, ofstream &fout)
+{
+  vertex vn = G.size();
+  vector<vertex> HCv(vn);
+  vector<bool> HCl(vn, false);
+  vector<index> ind(vn, 0);
+
+  index HCi = 0;
+  vertex v = 0;
+  HCv[HCi++] = v;
+  HCl[v] = true;
+
+
+  do {
+    while (ind[v] < G[v].size()) {
+      if (!HCl[G[v][ind[v]]]) {
+        v = G[v][ind[v]++];
+        HCv[HCi++] = v;
+        HCl[v] = true;
+      }
+      else ++ind[v];
+    }
+
+    if (HCi == vn) {
+      for (index i = 0; i < G[v].size(); ++i)
+        if (G[v][i] == 0) {
+          fout << "Hamiltonian cycle found: ";
+          for (index HCj = 0; HCj < vn; ++HCj)
+            fout << HCv[HCj] << "-";
+          fout << "0" << endl;
+
+          return true;
+        }
+    }
+
+    --HCi;
+    if (HCi == 0)
+      return 0;
+
+    HCl[v] = false;
+    ind[v] = 0;
+    v = HCv[HCi-1];
+  } while (true);
+
+}
+
+//
+//  Robert and Flores method for hamiltonian cycles.
+//
+index HamiltonianCycles(graph &G, ofstream &fout)
+{
+  vertex vn = G.size();
+  index HCn = 0;
+  vector<vertex> HCv(vn);
+  vector<bool> HCl(vn, false);
+  vector<index> ind(vn, 0);
+
+  index HCi = 0;
+  vertex v = 0;
+  HCv[HCi++] = v;
+  HCl[v] = true;
+
+  do {
+    while (ind[v] < G[v].size()) {
+      if (!HCl[G[v][ind[v]]]) {
+        v = G[v][ind[v]++];
+        HCv[HCi++] = v;
+        HCl[v] = true;
+      }
+      else ++ind[v];
+    }
+
+    if (HCi == vn) {
+      for (index i = 0; i < G[v].size(); ++i) {
+        if (G[v][i] == 0) {
+          ++HCn;
+          fout << "Hamiltonian cycle #" << HCn << " : ";
+          for (index HCj = 0; HCj < vn; ++HCj)
+            fout << HCv[HCj] << "-";
+          fout << "0" << endl;
+          break;
+        }
+      }
+    }
+
+    --HCi;
+    if (HCi == 0)
+      return HCn;
+
+    HCl[v] = false;
+    ind[v] = 0;
+    v = HCv[HCi-1];
+  } while (true);
+}
